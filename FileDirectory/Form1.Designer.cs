af8d07ba0c33d9bec3c0b7bcb4d53da4d141d595
Ayan Poddar <ayanpoddar0@gmail.com>
2021-01-16 17:52:01 +0530

    Add project files.

---
+++ FileDirectory/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace FileDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(13, 13);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(699, 23);
+            this.Input.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(719, 13);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(69, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Get";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(13, 42);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(775, 396);
+            this.Output.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox Output;
+    }
+}
+
---
+++ FileDirectory/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileDirectory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Input.Text))
+                Output.Text = SearchDirectory(Input.Text, 0);
+            else
+                MessageBox.Show("no directory found");
+        }
+
+        private string SearchDirectory(string path, uint depth)
+        {
+            var result = "";
+            var space = "";
+
+
+            for (uint i = 0; i < depth; i++)
+                space += "\t";
+            depth += 1;
+
+
+            foreach (var item in Directory.GetDirectories(path))
+            {
+                result += $"{space} {Path.GetFileName(item)} {Environment.NewLine}";
+                result += SearchDirectory(item, depth);
+            }
+
+
+            foreach (var item in Directory.GetFiles(path))
+                result += $"{space} {Path.GetFileName(item)} {Environment.NewLine}";
+
+            return result;
+        }
+    }
+}
